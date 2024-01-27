c61f5ec2297e369dded28c7eb3f4ae5cb69c32f6
aloknayak <56588590+Alok-Nayak@users.noreply.github.com>
2024-01-27 20:38:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -60,3 +60,5 @@ app.listen(PORT, () => {
   console.log(`Server is running on port : ${PORT}`);
 });
 //test
+
+// db url: mongodb+srv://admin:admin@cluster0.mveunki.mongodb.net/TODO-APP-DB?retryWrites=true&w=majority
